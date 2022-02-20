6e3d038cd51166e84309a0219ee58cbbbeb180a1
Joydev Das <iamjoydevdas@gmail.com>
2022-02-20 12:25:18 +0530

    init

---
+++ GangOfFour/creational/src/main/java/tutorial/abengalicoder/creational/App.java
@@ -0,0 +1,13 @@
+package tutorial.abengalicoder.creational;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
